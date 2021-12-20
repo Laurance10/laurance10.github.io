a9f1d867c4b05d9c50629a285fb4c823cff81254
Victor Laurance <victorlrd13@hotmail.com>
2021-12-20 15:27:17 -0300

    Subindo portfolio

---
+++ public/project.js
@@ -0,0 +1,33 @@
+let projects = [
+    {
+        name: "Busca de Mangás",
+        tags: "#javascript, #fullstack, #ui/ux, #backend",
+        image: "project (1).png",
+    },
+    {
+        name: "Busca CEP",
+        tags: "#javascript, #fullstack",
+        image: "project (2).png",
+    },
+    {
+        name: "Pokédex",
+        tags: "#java, #fullstack",
+        image: "project (3).png",
+    },
+    {
+        name: "Contador em Flutter/Dart",
+        tags: "#flutter, #fullstack",
+        image: "project (4).png",
+    },
+    {
+        name: "PayFlow",
+        tags: "#flutter, #fullstack",
+        image: "project (5).png",
+    },
+    {
+        name: "Calculadora de IMC",
+        tags: "#javascript, #fullstack",
+        image: "project (6).png",
+    },
+    // +8 more
+]
